229ca3806fc53aa4d99838f4c58e5fcb8670cc10
Carlos Germano <germano.carlos2712@gmail.com>
2021-09-09 20:45:28 -0300

    Add

--- Fontes/EasyCine.Kernel/Util/Constantes.cs
+++ Fontes/EasyCine.Kernel/Util/Constantes.cs
@@ -6,10 +6,8 @@ using System.IO;
 
 namespace EasyCine.Kernel.Util
 {
-	public class Constantes
+	public static class Constantes
 	{
-		//<#keep(conteudo)#>
-		public static string ConnectionString = "";
-		//<#/keep(conteudo)#>
+		public static string ConnectionString = "server=database-1.cmu57yqd5fbu.sa-east-1.rds.amazonaws.com;port=3306;database=cinema;uid=cine_app;password=EZfQkg18@$uA;";
 	}
 }
