bc845cecc37d76a445e2c93483f8d149dec631d4
Carlos Germano <germano.carlos2712@gmail.com>
2022-05-15 16:58:39 -0300

    Updates !

---
+++ Fontes/EasyCine.Tests/TransactionTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace EasyCine.Tests;
+
+public class TransactionTests
+{
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(1)] // Gera erro !
+    [InlineData(3)]
+    public void IsNotEven(int value)
+    {
+        var result = value % 2 == 0;
+        Assert.False(result, $"{value} should not be prime");
+    }
+}
